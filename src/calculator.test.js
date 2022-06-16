ad588b75d7d249272a7a8b1983063cac0b89452a
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-06-16 12:57:23 +0530

    Update calculator.test.js

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -41,7 +41,7 @@ test("Added by arjun", () => {
 
 test('adding should should detect odd number--', () => {
     const number = Math.floor(Math.random() * 10);
-    const result = 11 % 2;
+    const result = number % 2;
     expect(result).toBe(1);
 });
 
